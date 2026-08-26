2735a7092d105ebb534e0fa4f1588e08a37e6f0e
Roy <hidiho@gmail.com>
2026-08-26 15:42:57 +0200

    Fix instantiation of NLWeatherAutoEDRCoordinator

--- custom_components/nl_weather/__init__.py
+++ custom_components/nl_weather/__init__.py
@@ -67,7 +67,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: NLWeatherConfigEntry) ->
         )
         if subentry.data[CONF_MODE] == StationMode.AUTO:
             entry.runtime_data.edr_coordinators[subentry_id] = (
-                NLWeatherAutoEDRCoordinator(hass, subentry, ns, edr)
+                NLWeatherAutoEDRCoordinator(hass, entry, subentry, ns, edr)
             )
         elif subentry.data[CONF_MODE] == StationMode.MANUAL:
             entry.runtime_data.edr_coordinators[subentry_id] = (
